f437542fe13e4cffee989c101f64919f521962d7
SrGodoy <115853981+SrGodoy@users.noreply.github.com>
2023-10-31 11:40:54 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -16,3 +16,39 @@ swiper.on('touchStart', function() {
 swiper.on('touchEnd', function() {
   swiper.autoplay.start();
 });
+
+italianAudio.volume = 0.5;
+
+playButton.addEventListener("click", function() {
+  if (!isPlaying) {
+    italianAudio.currentTime = audioCurrentTime; // Define o tempo de reprodução
+    italianAudio.play();
+    playButton.innerText = "Pausar Música";
+  } else {
+    italianAudio.pause();
+    playButton.innerText = "Tocar Música";
+  }
+  isPlaying = !isPlaying; // Inverte o estado
+});
+
+italianAudio.addEventListener("timeupdate", function() {
+  audioCurrentTime = italianAudio.currentTime; // Atualiza o tempo de reprodução atual
+});
+
+// Adicionar um evento para salvar o estado e tempo de reprodução quando o usuário sair da página
+window.addEventListener("beforeunload", function() {
+  localStorage.setItem("audioPlaying", isPlaying);
+  localStorage.setItem("audioCurrentTime", audioCurrentTime);
+});
+
+// Verificar o estado da música e o tempo de reprodução quando a página é carregada
+window.addEventListener("load", function() {
+  const storedState = localStorage.getItem("audioPlaying");
+  if (storedState === "true") {
+    const storedTime = localStorage.getItem("audioCurrentTime");
+    italianAudio.currentTime = parseFloat(storedTime);
+    italianAudio.play();
+    playButton.innerText ="Pausar Música";
+    isPlaying = true;
+  }
+});
